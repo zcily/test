0373d3fbb5baee57e95298977231e6cc2a35726c
zhangwuyi <zhangwuyi@sunniwell.net>
2014-07-30 09:35:57 +0800

    add unix program

--- unix/stat.cpp
+++ unix/stat.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
@@ -20,7 +21,43 @@ void showFileInfor(const char* direntName, const char* fileName)
 		sprintf(completeFileName, "%s%s", direntName, fileName);
 
 	cout << completeFileName << endl;
-	lstat(".", &st);
+	if(lstat(".", &st) == -1) {
+		perror("stat error!!!");	
+		return;	
+	}
+
+	printf("File type:                ");
+
+	switch (st.st_mode & S_IFMT) {
+	case S_IFBLK:  printf("block device\n");            break;
+	case S_IFCHR:  printf("character device\n");        break;
+	case S_IFDIR:  printf("directory\n");               break;
+	case S_IFIFO:  printf("FIFO/pipe\n");               break;
+	case S_IFLNK:  printf("symlink\n");                 break;
+	case S_IFREG:  printf("regular file\n");            break;
+	case S_IFSOCK: printf("socket\n");                  break;
+	default:       printf("unknown?\n");                break;
+	}
+
+	printf("I-node number:            %ld\n", (long) st.st_ino);
+
+	printf("Mode:                     %lo (octal)\n",
+		(unsigned long) st.st_mode);
+
+	printf("Link count:               %ld\n", (long) st.st_nlink);
+	printf("Ownership:                UID=%ld   GID=%ld\n",
+		(long) st.st_uid, (long) st.st_gid);
+
+	printf("Preferred I/O block size: %ld bytes\n",
+		(long) st.st_blksize);
+	printf("File size:                %lld bytes\n",
+		(long long) st.st_size);
+	printf("Blocks allocated:         %lld\n",
+		(long long) st.st_blocks);
+
+	printf("Last status change:       %s", ctime(&st.st_ctime));
+	printf("Last file access:         %s", ctime(&st.st_atime));
+	printf("Last file modification:   %s", ctime(&st.st_mtime));
 }
 
 void myLs(const char* dirName)
@@ -41,6 +78,7 @@ void myLs(const char* dirName)
 	}
 }
 
+int a = 0;
 int main(int argc, char**argv)
 {	
 	if(!argv[1])
