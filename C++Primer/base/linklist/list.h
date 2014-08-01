434180d0ac7e9e0acfbb652e700730ad98355464
zhangwuyi <zhangwuyi@sunniwell.net>
2014-08-01 15:08:30 +0800

    complete link list

--- C++Primer/base/linklist/list.h
+++ C++Primer/base/linklist/list.h
@@ -36,22 +36,29 @@ namespace MyList
 			bool find_value(const T& value);
 
 			bool clear_all_value();
+
+			unsigned int list_length();
+
+			void show();
 	};
 	
 	template<typename T>
 	linkList<T>::linkList()
-		:first(new Node)
 	{
-		if(this->first)
-			this->first->value = 0;
+		first = NULL;
 	}
 	
+	//*******************
 	template<typename T>
 	linkList<T>::linkList(T value)
-		:first(new Node)
 	{
-		if(this->first)	
-			this->first->value = value;
+		Node*temp = new Node;
+		if(temp)	{
+			temp->value = value;
+			temp->next = NULL;
+		}	
+
+		first = temp;
 	}
 			
 	template<typename T>
@@ -80,6 +87,15 @@ namespace MyList
 	template<typename T>
 	bool linkList<T>::push_head(const T& value)
 	{
+		Node *newNode = new Node;
+		if(!newNode){
+			cout << "new error"	 << endl;
+			return false;
+		}
+		
+		newNode->value = value;
+		newNode->next = first;
+		first = newNode;
 	
 		return true;
 	}
@@ -87,13 +103,49 @@ namespace MyList
 	template<typename T>
 	bool linkList<T>::push_tail(const T& value)
 	{
+		Node*index = first;
+
+		while(index && index->next){
+			index = index->next;	
+		}
+		
+		Node *newNode = new Node;
+		if(!newNode){
+			cout << "new error"	 << endl;
+			return false;
+		}
+		newNode->value = value;
+		newNode->next = NULL;
+
+		if(!index)
+			first = newNode;
+		else
+			index->next = newNode;	
 	
 		return true;
 	}
 
 	template<typename T>
-	bool linkList<T>::push_value_by_postion(const T& value, const T&)
+	bool linkList<T>::push_value_by_postion(const T& value, const T& postionValue)
 	{
+		Node*index  = first;
+
+		while(index) {
+			if(index->value == postionValue)
+				break;
+			index = index->next;		
+		}
+
+		if(!index){
+			cout << "not find the value"<< endl;
+			return false;
+		}
+
+		Node* newNode = new Node;
+		newNode->value = value;
+
+		newNode->next = index->next;
+		index->next = newNode;
 	
 		return true;
 	}
@@ -101,13 +153,37 @@ namespace MyList
 	template<typename T>
 	bool linkList<T>::delete_value(const T& value)
 	{
-	
+		Node* index = first;	
+		if(!index)
+			return false;
+
+		while(index->next){
+			if(index->next->value == value) {
+				Node*temp = index->next;
+				index->next = index->next->next;
+				
+				delete temp;
+				temp = NULL;
+			}
+			index = index->next;
+		}
+
 		return true;
 	}
 
 	template<typename T>
 	bool linkList<T>::modify_value(const T& orignalValue, const T&replaceValue)
 	{
+		Node* index = first;
+		if(!index)
+			return false;
+
+		while(index){
+			if(index->value == orignalValue){
+				index->value = replaceValue;	
+			}
+			index = index->next;
+		}
 	
 		return true;
 	}
@@ -115,6 +191,19 @@ namespace MyList
 	template<typename T>
 	bool linkList<T>::find_value(const T& value)
 	{
+		Node* index = first;
+		if(!index)
+			return false;
+
+		while(index){
+			if(index->value == value){
+				break;
+			}
+			index = index->next;
+		}
+
+		if(!index)
+			return false;
 	
 		return true;
 	}
@@ -122,8 +211,31 @@ namespace MyList
 	template<typename T>
 	bool linkList<T>::clear_all_value()
 	{
+		Node* temp = NULL;
+
+		while(first){
+			temp = first;
+			first = first->next;
+			delete temp;
+		}
 	
 		return true;
+	}
+
+	template<typename T>
+	void linkList<T>::show()
+	{
+		Node*index = first;
+		if(!index){
+			cout << "no value"<< endl;
+			return;
+		}
+
+		while(index){
+			cout << "__" << index->value;
+			index = index->next;
+		}
+		cout << endl;
 	}
 }
 
--- C++Primer/base/linklist/main.cpp
+++ C++Primer/base/linklist/main.cpp
@@ -7,7 +7,37 @@ using namespace MyList;
 
 int main(int argc, char**argv)
 {
-	linkList<int> temp;		
+	linkList<int> temp(10);		
+
+	for(int i = 55; i<60; ++i)
+		temp.push_tail(i);
+
+	for(int i = 10; i < 15; ++i)
+		temp.push_head(i);
+
+	temp.push_value_by_postion(101, 58);
+
+	temp.show();
+
+	temp.delete_value(13);
+
+	temp.show();
+
+	temp.modify_value(10, 11111);
+	temp.show();
+
+	int findValue = 0;
+	cin >> findValue;
+
+	cout << "want to find value : " << findValue << "   : " << (temp.find_value(findValue) ? "true" : "false") << endl;
+
+	temp.clear_all_value();
+
+	temp.show();
+
+	temp.push_value_by_postion(151, 58);
+
+	temp.show();
 
 	return EXIT_SUCCESS;
 }
